d637789b83931f5e7e87cc2e480b2a91fd101b97
”EvanO3” <evan.oni@hotmail.com>
2026-03-25 20:05:28 -0400

    retrieve user id from context

--- src/main/java/com/Novara/Budgeting/Service/TransactionServiceImpl.java
+++ src/main/java/com/Novara/Budgeting/Service/TransactionServiceImpl.java
@@ -4,11 +4,15 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.Novara.Budgeting.DTOs.TransactionDTO;
+import com.Novara.Budgeting.Model.Source;
 import com.Novara.Budgeting.Model.TransactionModel;
 import com.Novara.Budgeting.Repository.TransactionRepository;
 
@@ -20,6 +24,7 @@ import lombok.RequiredArgsConstructor;
     -
 */
 public class TransactionServiceImpl implements TransactionService {
+    private final Logger logger = LoggerFactory.getLogger(TransactionServiceImpl.class);
     private final  TransactionRepository transactionRepository;
    
     private final ModelMapper modelMapper;
@@ -30,19 +35,26 @@ public class TransactionServiceImpl implements TransactionService {
     }
 
 
-    @Transactional
-    public TransactionDTO createTransaction(TransactionDTO transactionDTO, UUID profileId) {
+    public TransactionDTO createTransaction(TransactionDTO transactionDTO) {
         if(transactionDTO == null){
             throw new IllegalArgumentException("Transaction cannot be null");
         }
+
+        String principal = SecurityContextHolder.getContext()
+                        .getAuthentication()
+                        .getPrincipal().
+                         toString();
+
+        UUID authId = UUID.fromString(principal);
         TransactionModel transaction = modelMapper.map(transactionDTO, TransactionModel.class);
-        transaction.setAuthId(profileId);
+        transaction.setAuthId(authId);
         TransactionModel savedTransaction = transactionRepository.save(transaction);
         return modelMapper.map(savedTransaction, TransactionDTO.class);
     }
 
 
 
+    
     
 
 
