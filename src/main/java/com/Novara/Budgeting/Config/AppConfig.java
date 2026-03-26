531a7cc57f384f097835e3116d8ff2b4f882c491
”EvanO3” <evan.oni@hotmail.com>
2026-03-25 22:13:36 -0400

    added model mapping config for skipping nulls and updating

--- src/main/java/com/Novara/Budgeting/Config/AppConfig.java
+++ src/main/java/com/Novara/Budgeting/Config/AppConfig.java
@@ -4,12 +4,23 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+
+    /*Since a bean with the same name is created using qualifier in the other class
+    will allow for this bea to be used */
+    @Bean("updateModelMapper")
+    public ModelMapper updateModelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        return modelMapper;
     }
     
 }
