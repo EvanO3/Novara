f6fdc4b498716b2371537e44052be04fc5dd2257
”EvanO3” <evan.oni@hotmail.com>
2026-03-25 22:15:20 -0400

    Transaction response for getting transaction info

--- src/main/java/com/Novara/Budgeting/DTOs/TransactionResponse.java
+++ src/main/java/com/Novara/Budgeting/DTOs/TransactionResponse.java
@@ -1,5 +1,22 @@
 package com.Novara.Budgeting.DTOs;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TransactionResponse {
     
+    private List<TransactionDTO> content;
+
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Integer totalPages;
+    private Long totalElements;
+    private boolean lastPage;
+
 }
